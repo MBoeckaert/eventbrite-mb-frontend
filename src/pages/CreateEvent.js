4c9c0de22ecc285cd739cd7c89b0c493aaaa8857
Matthias <matthiasboeckaert@gmail.com>
2022-08-14 17:32:39 +0200

    first try to write to db

--- src/pages/CreateEvent.js
+++ src/pages/CreateEvent.js
@@ -1,12 +1,29 @@
 import React from "react";
 import { Container } from "@mui/system";
-import { Stack, TextField, Typography, Button } from "@mui/material";
+import {
+  Stack,
+  TextField,
+  Typography,
+  Button,
+  Snackbar,
+  Alert,
+} from "@mui/material";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { backendUrl } from "../lib/functions";
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "react-query";
+import { useStore } from "../store";
 
 import { styled } from "@mui/material/styles";
 // import { useState } from "react";
+
+const defaultValues = {
+  categories: [],
+  name: "",
+  description: "",
+};
 
 const CreateButton = styled(Button)({
   width: "100%",
@@ -23,9 +40,60 @@ const CreateButton = styled(Button)({
 });
 
 const CreateEvent = () => {
-  const [value, setValue] = React.useState(new Date("2014-08-18T21:11:54"));
+  const [value, setValue] = React.useState(new Date("2022-01-01T00:00:00"));
   const handleChange = (newValue) => {
     setValue(newValue);
+  };
+
+  const {
+    handleSubmit,
+    formState: { errors },
+    register,
+    reset,
+  } = useForm({ defaultValues });
+
+  const jwt = useStore((state) => state.jwt);
+
+  const queryClient = useQueryClient();
+  // const { isLoading, data: categories } = useQuery("categories", async () => {
+  //   const data = await fetch(`${backendUrl}/api/categories`).then((r) =>
+  //     r.json()
+  //   );
+  //   return data;
+  // });
+
+  const postEvent = async (data) => {
+    return await fetch(`${backendUrl}/api/events`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify(data),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        if (data.error) {
+          throw data.error;
+        }
+        return data;
+      });
+  };
+
+  const mutation = useMutation(postEvent, {
+    onSuccess: () => {
+      console.log("success");
+      queryClient.invalidateQueries("events");
+      reset();
+    },
+  });
+
+  const onSubmit = (data) => {
+    mutation.mutate({ data });
+  };
+
+  const handleCloseSnackbar = () => {
+    mutation.reset();
   };
 
   return (
@@ -38,8 +106,17 @@ const CreateEvent = () => {
         Create your Event!
       </Typography>
 
-      <Stack spacing={4}>
-        <TextField id="name" label="Name" required />
+      <Stack spacing={4} as="form" noValidate onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          id="name"
+          label="Name"
+          required
+          error={!!errors?.name}
+          helperText={errors?.name?.message}
+          {...register("name", {
+            required: "Name is required",
+          })}
+        />
         <LocalizationProvider dateAdapter={AdapterDateFns}>
           <DesktopDatePicker
             label="Datum"
@@ -49,8 +126,26 @@ const CreateEvent = () => {
             renderInput={(params) => <TextField {...params} />}
           />
         </LocalizationProvider>
-        <TextField id="location" label="Location" required />
-        <TextField id="price" label="Price" required />
+        <TextField
+          id="location"
+          label="Location"
+          required
+          error={!!errors?.name}
+          helperText={errors?.name?.message}
+          {...register("location", {
+            required: "Location is required",
+          })}
+        />
+        <TextField
+          id="price"
+          label="Price"
+          required
+          error={!!errors?.name}
+          helperText={errors?.name?.message}
+          {...register("price", {
+            required: "Price is required",
+          })}
+        />
         <Typography>Create A File Upload here</Typography>
         <Button variant="contained" component="label">
           Upload Event Image
@@ -62,10 +157,18 @@ const CreateEvent = () => {
           multiline
           rows={4}
           maxRows={6}
+          error={!!errors?.name}
+          helperText={errors?.name?.message}
+          {...register("description", {
+            required: "Description is required",
+          })}
         />
         <CreateButton
+          loading={mutation.isLoading}
+          loadingIndicator="Adding restaurant"
+          type="submit"
           color="inherit"
-          component="a"
+          // component="a"
           // href={`${backendUrl}/api/connect/google`}
           sx={{
             mt: 3,
@@ -76,6 +179,27 @@ const CreateEvent = () => {
         >
           Create Event
         </CreateButton>
+        <Snackbar
+          open={mutation.isSuccess}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          autoHideDuration={3000}
+          onClose={handleCloseSnackbar}
+        >
+          <Alert severity="success" sx={{ width: "100%" }}>
+            Restaurant added
+          </Alert>
+        </Snackbar>
+        <Snackbar
+          open={mutation.isError}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          autoHideDuration={3000}
+          onClose={handleCloseSnackbar}
+        >
+          <Alert severity="error" sx={{ width: "100%" }}>
+            Restaurant could not be added: <br />
+            {mutation.error?.message}
+          </Alert>
+        </Snackbar>
       </Stack>
     </Container>
   );
