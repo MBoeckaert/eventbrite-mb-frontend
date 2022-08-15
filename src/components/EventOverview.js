30a4851c75977987bdcd080e3f9f53340c4e9869
Matthias <matthiasboeckaert@gmail.com>
2022-08-15 17:57:13 +0200

    width container 65%

--- src/components/EventOverview.js
+++ src/components/EventOverview.js
@@ -33,18 +33,9 @@ const EventOverview = (props) => {
   return (
     <>
       <Card variant="outlined" sx={{ cursor: "pointer" }}>
-        <Grid
-          container
-          spacing={2}
-          sx={{
-            // height: "60%",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-around",
-          }}
-        >
+        <Grid container spacing={2}>
           <Link to={`/eventInfo/${props.id}`} underline="none">
-            <Grid item container spacing={2} component="span">
+            <Grid item container spacing={2} component="span" width="65%">
               <Grid item>
                 {/* <img
               src={`${backendUrl + props.picture} `}
