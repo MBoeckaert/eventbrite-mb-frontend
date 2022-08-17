bfd344af99c806438b3f4f438cce66e565195dc7
Matthias <matthiasboeckaert@gmail.com>
2022-08-17 17:32:34 +0200

    reinstalled qs

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -69,14 +69,14 @@ export default function SignIn() {
 
   console.log(profile);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-  };
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   const data = new FormData(event.currentTarget);
+  //   console.log({
+  //     email: data.get("email"),
+  //     password: data.get("password"),
+  //   });
+  // };
 
   return (
     <ThemeProvider theme={theme}>
@@ -98,7 +98,7 @@ export default function SignIn() {
             </Typography>
             <Box
               component="form"
-              onSubmit={handleSubmit}
+              // onSubmit={handleSubmit}
               noValidate
               sx={{ mt: 1 }}
             >
