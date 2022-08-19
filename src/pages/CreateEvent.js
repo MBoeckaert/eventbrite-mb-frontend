52655762e41f42ce08934aa67a43bdfe48c56a35
Matthias <matthiasboeckaert@gmail.com>
2022-08-19 15:53:42 +0200

    adding event to next data prop in line, step by step

--- src/pages/CreateEvent.js
+++ src/pages/CreateEvent.js
@@ -102,7 +102,7 @@ const CreateEvent = () => {
     console.log(data.event.image);
     console.log(data.event.image.length);
     if (data.event.image.length > 0) {
-      formData.append("files.cover", data.image[0], data.image[0].name);
+      formData.append("files.cover", data.event.image[0], data.image[0].name);
     }
     formData.append("data", JSON.stringify({ ...data, image: null }));
     // parseInt(data.price);
