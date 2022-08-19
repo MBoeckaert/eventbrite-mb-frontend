fd0b9787c453e4143f1a4c878b167a9cc17318fd
Matthias <matthiasboeckaert@gmail.com>
2022-08-19 15:37:28 +0200

    log data.event bec length is undefined

--- src/pages/CreateEvent.js
+++ src/pages/CreateEvent.js
@@ -98,6 +98,7 @@ const CreateEvent = () => {
     //still need to add new FormData for uploading files
     const formData = new FormData();
     console.log(data);
+    console.log(data.event.image);
     if (data.image.length > 0) {
       formData.append("files.cover", data.image[0], data.image[0].name);
     }
