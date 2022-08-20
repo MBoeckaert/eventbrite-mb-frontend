a4665db8b83a43c99073a257513bf1cd3b0cabea
Matthias <matthiasboeckaert@gmail.com>
2022-08-20 13:41:03 +0200

    proper img notation

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -87,7 +87,7 @@ const Home = () => {
                     {/* <Link to={`/eventInfo/${event.id}`} underline="none"> */}
                     {console.log(event.id)}
                     <EventOverview
-                      image={event.attributes.image.data}
+                      image={event.attributes.image.data.attributes.name}
                       id={event.id}
                       name={event.attributes.name}
                       date={event.attributes.date}
